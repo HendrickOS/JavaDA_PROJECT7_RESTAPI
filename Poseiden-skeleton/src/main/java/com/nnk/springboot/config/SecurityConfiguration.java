e626d431c0b89b525dc2c59b1eaec39925a5d1e8
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-01-31 18:33:07 +0100

    Restore package config

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.nnk.springboot.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/home/**", "/user/**").permitAll()
+				.antMatchers("/bidList/**", "/curvePoint/**", "/rating/**", "/ruleName/**", "/trade/**")
+				.access("hasRole('USER')").anyRequest().authenticated().and().formLogin().and().logout()
+				.logoutSuccessUrl("/home/**").and().httpBasic();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema().withUser("user")
+				.password(passwordEncoder().encode("password")).roles("USER");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
