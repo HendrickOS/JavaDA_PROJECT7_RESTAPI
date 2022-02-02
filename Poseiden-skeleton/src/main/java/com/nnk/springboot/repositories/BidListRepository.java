48ffc2276671024df57e15bbd717e49416de47d6
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-02-02 11:32:34 +0100

    Add extends CrudRepository<> in User, Trade, Curvepoint, Rating & BidList

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/BidListRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/BidListRepository.java
@@ -1,9 +1,10 @@
 package com.nnk.springboot.repositories;
 
-import com.nnk.springboot.domain.BidList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
+import com.nnk.springboot.domain.BidList;
 
-public interface BidListRepository extends JpaRepository<BidList, Integer> {
+public interface BidListRepository extends JpaRepository<BidList, Integer>, CrudRepository<BidList, Integer> {
 
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/CurvePointRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/CurvePointRepository.java
@@ -1,9 +1,10 @@
 package com.nnk.springboot.repositories;
 
-import com.nnk.springboot.domain.CurvePoint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
+import com.nnk.springboot.domain.CurvePoint;
 
-public interface CurvePointRepository extends JpaRepository<CurvePoint, Integer> {
+public interface CurvePointRepository extends JpaRepository<CurvePoint, Integer>, CrudRepository<CurvePoint, Integer> {
 
 }
