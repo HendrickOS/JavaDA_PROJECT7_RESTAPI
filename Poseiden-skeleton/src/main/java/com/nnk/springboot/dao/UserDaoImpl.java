84b19b35e545ca29bd4fbe506aa9e126303e3a5e
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-02-03 18:03:46 +0100

    Add UserDaoImpl

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/dao/UserDaoImpl.java
@@ -0,0 +1,44 @@
+package com.nnk.springboot.dao;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.repositories.UserRepository;
+
+@Component
+public class UserDaoImpl implements UserDao {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public User save(User user) {
+		return userRepository.save(user);
+	}
+
+	@Override
+	public List<User> findAll() {
+		List<User> result = new ArrayList<>();
+		Iterable<User> findAll = userRepository.findAll();
+		for (Iterator<User> iterator = findAll.iterator(); iterator.hasNext();) {
+			result.add(iterator.next());
+		}
+		return result;
+	}
+
+	@Override
+	public User findByUsername(String username) {
+		return userRepository.findByUsername(username);
+	}
+
+	@Override
+	public User findById(Integer id) {
+		return userRepository.findById(id).get();
+	}
+
+}
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/UserRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.nnk.springboot.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
@@ -8,5 +10,9 @@ import com.nnk.springboot.domain.User;
 
 public interface UserRepository
 		extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User>, CrudRepository<User, Integer> {
+
+	User findByUsername(String username);
+
+	Optional<User> findById(Integer id);
 
 }
