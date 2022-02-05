dcbe894873b5e530593eb5fa0ce0f8a909c8882b
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-02-05 04:59:30 +0100

    Clean

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/misc/DataInitializr.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/misc/DataInitializr.java
@@ -3,6 +3,7 @@ package com.nnk.springboot.misc;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,9 @@ public class DataInitializr {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@PostConstruct
 	public void init() {
