b51ae72967a285f1246a733bef749d5ba1d96833
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-02-09 17:47:59 +0100

    Complete Entities's attributes

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -1,10 +1,13 @@
 package com.nnk.springboot.domain;
 
+import java.security.Timestamp;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "curvepoint")
@@ -12,5 +15,59 @@ public class CurvePoint {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
-	// TODO: Map columns in data table CURVEPOINT with corresponding java fields
+	@NotNull
+	private Integer curveId;
+	private Timestamp asOfDate;
+	private Double term;
+	private Double value;
+	private Timestamp creationDate;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getCurveId() {
+		return curveId;
+	}
+
+	public void setCurveId(Integer curveId) {
+		this.curveId = curveId;
+	}
+
+	public Timestamp getAsOfDate() {
+		return asOfDate;
+	}
+
+	public void setAsOfDate(Timestamp asOfDate) {
+		this.asOfDate = asOfDate;
+	}
+
+	public Double getTerm() {
+		return term;
+	}
+
+	public void setTerm(Double term) {
+		this.term = term;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	public Timestamp getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Timestamp creationDate) {
+		this.creationDate = creationDate;
+	}
+
 }
