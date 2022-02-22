e9e4d426f38dbc323a88438d2ddee51e4d224138
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-02-23 00:07:50 +0100

    PasswordRules Annotation

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/PasswordRules.java
@@ -0,0 +1,8 @@
+package com.nnk.springboot.domain;
+
+public @interface PasswordRules {
+
+//	valider la saisie, en particulier pour les champs numériques (comprenant uniquement des chiffres) et 
+//	pour les mots de passe (au moins une lettre majuscule, au moins 8 caractères, au moins un chiffre et un symbole)
+
+}
